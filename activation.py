a288bd069eea9dfabfeb3dd72e15caa70ed01c0c
Bruno Gonçalves <bgoncalves@gmail.com>
2017-02-12 22:35:04 -0500

    Code cleanup

--- activation.py
+++ activation.py
@@ -8,51 +8,66 @@ def linear(z):
     return z
 
 
-def binary(z):
-    return np.where(z > 0, 1, 0)
+def linearGradient(z):
+    return np.ones(z.shape)
 
 
 def relu(z):
     return np.where(z > 0, z, 0)
 
 
+def reluGradient(z):
+    return np.where(z > 0, 1, 0)
+
+
 def sigmoid(z):
     return 1./(1+np.exp(-z))
 
 
+def sigmoidGradient(z):
+    h = sigmoid(z)
+    return h*(1-h)
+
+
 def tanh(z):
     return np.tanh(z)
+
+
+def tanhGradient(z):
+    return 1-np.power(tanh(z), 2.0)
+
 
 z = np.linspace(-6, 6, 100)
 
 plt.style.use('ggplot')
 
 plt.plot(z, linear(z), 'r-')
+plt.plot(z, linearGradient(z), 'b-')
 plt.xlabel('z')
 plt.title('Linear activation function')
+plt.legend(['function', 'gradient'])
 plt.savefig('figs/linear.png')
 plt.close()
 
-plt.plot(z, binary(z), 'r-')
-plt.xlabel('z')
-plt.title('Binary activation function')
-plt.ylim([-0.01, 1.01])
-plt.savefig('figs/binary.png')
-plt.close()
-
 plt.plot(z, relu(z), 'r-')
+plt.plot(z, reluGradient(z), 'b-')
+plt.legend(['function', 'gradient'])
 plt.xlabel('z')
 plt.title('Rectified Linear activation function')
 plt.savefig('figs/relu.png')
 plt.close()
 
 plt.plot(z, sigmoid(z), 'r-')
+plt.plot(z, sigmoidGradient(z), 'b-')
+plt.legend(['function', 'gradient'])
 plt.xlabel('z')
 plt.title('Sigmoid activation function')
 plt.savefig('figs/sigmoid.png')
 plt.close()
 
 plt.plot(z, tanh(z), 'r-')
+plt.plot(z, tanhGradient(z), 'b-')
+plt.legend(['function', 'gradient'])
 plt.xlabel('z')
 plt.title('Hyperbolic Tangent activation function')
 plt.savefig('figs/tanh.png')
--- forward.py
+++ forward.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 import numpy as np
+from activation import sigmoid
 
 
 def forward(Theta, X, active):
-    m = X.shape[0]
+    N = X.shape[0]
 
     # Add the bias column
-    X_ = np.concatenate((np.ones((m, 1)), X), 1)
+    X_ = np.concatenate((np.ones((N, 1)), X), 1)
 
     # Multiply by the weights
     z = np.dot(X_, Theta.T)
@@ -17,10 +18,25 @@ def forward(Theta, X, active):
 
     return a
 
+
+def predict(Theta1, Theta2, X):
+    h1 = forward(Theta1, X, sigmoid)
+    h2 = forward(Theta2, h1, sigmoid)
+
+    return np.argmax(h2, 1)
+
+
+def accuracy(y_, y):
+    return np.mean((y_ == y.flatten()))*100.
+
+
 if __name__ == "__main__":
     Theta1 = np.load('input/Theta1.npy')
-    X = np.load('input/X_train.npy')[:10]
+    Theta2 = np.load('input/Theta2.npy')
 
-    from activation import sigmoid
+    X = np.load('input/X_train.npy')
+    y = np.load('input/y_train.npy')
 
-    active_value = forward(Theta1, X, sigmoid)
+    y_ = predict(Theta1, Theta2, X)
+
+    print(accuracy(y_, y))
